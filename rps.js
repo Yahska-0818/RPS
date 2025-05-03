fed056688730698ab7295132bfd211a566c15396
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-03 17:55:16 +0000

    Implemented user input to get human choice for rps

--- rps.js
+++ rps.js
@@ -18,4 +18,19 @@ function getComputerChoice(number) {
     }
 }
 
-console.log(getComputerChoice(getRandomNumber(1,4)))
+function getHumanChoice() {
+    let userChoice = parseInt(prompt("Type 1 for Rock, 2 for Paper or 3 for Scissors to make a choice"))
+    if (userChoice === 1) {
+        return "Rock"
+    }
+
+    else if (userChoice === 2) {
+        return "Paper"
+    }
+
+    else if (userChoice === 3) {
+        return "Scissors"
+    }
+}
+
+console.log(getHumanChoice());
