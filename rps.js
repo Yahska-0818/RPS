70c3db3727df8da48d202bfe18b28578cb66f689
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-05 07:25:33 +0000

    Refactored code and added icon images

--- rps.js
+++ rps.js
@@ -4,6 +4,35 @@ const startMain = document.querySelector(".main")
 const header = document.querySelector(".header")
 const footer = document.querySelector(".footer")
 
+// adding divs for new ui
+const textDiv = document.createElement("div")
+const buttonDiv = document.createElement("div")
+const actionDiv = document.createElement("div")
+
+// adding content to new ui
+const textH1 = document.createElement("h1")
+
+// adding buttons
+const rockButton = document.createElement("button")
+const paperButton = document.createElement("button")
+const scissorButton = document.createElement("button")
+
+//main action area
+
+const userCont = document.createElement("div")
+const userText = document.createElement("p")
+const imgOne = document.createElement("img")
+const compCont = document.createElement("div")
+const compText = document.createElement("p")
+const imgTwo = document.createElement("img")
+const userScore = document.createElement("p")
+const compScore = document.createElement("p")
+
+// keeping score
+let scoreOne = 0
+let scoreTwo = 0
+
+
 startButton.addEventListener("click", ()=> {
 
     // changing existing ui
@@ -12,46 +41,25 @@ startButton.addEventListener("click", ()=> {
     header.style.height = "1vh"
     footer.style.height = "1vh"
 
-    // adding divs for new ui
-    const textDiv = document.createElement("div")
-    const buttonDiv = document.createElement("div")
-    const actionDiv = document.createElement("div")
-
-    // adding content to new ui
-    const textH1 = document.createElement("h1")
     textH1.textContent = "Make a move Gunslinger!"
     textH1.style.cssText = "color:white; fontSize = 2vh; margin: 0;"
     textDiv.appendChild(textH1)
     startMain.appendChild(textDiv)
 
-
-    const rockButton = document.createElement("button")
     rockButton.textContent = "Rock"
     rockButton.style.cssText = `width: 15vh; height: 5vh; fontFamily: "Winky Rough", sans-serif; borderRadius: 1vh; padding: 0; fontSize: 3vh; color: #540B05; cursor: pointer; borderColor: #540B05;`
     buttonDiv.appendChild(rockButton)
-    
-    const paperButton = document.createElement("button")
+
     paperButton.textContent = "Paper"
     paperButton.style.cssText = `width: 15vh; height: 5vh; fontFamily: "Winky Rough", sans-serif; borderRadius: 1vh; padding: 0; fontSize: 3vh; color: #540B05; cursor: pointer; borderColor: #540B05;`
     buttonDiv.appendChild(paperButton)
-    
-    const scissorButton = document.createElement("button")
+
     scissorButton.textContent = "Scissor"
     scissorButton.style.cssText = `width: 15vh; height: 5vh; fontFamily: "Winky Rough", sans-serif; borderRadius: 1vh; padding: 0; fontSize: 3vh; color: #540B05; cursor: pointer; borderColor: #540B05;`
     buttonDiv.appendChild(scissorButton)
     
-    
     buttonDiv.style.cssText = "display:flex; gap: 2vh;"
     startMain.appendChild(buttonDiv)
-    
-    const userCont = document.createElement("div")
-    const userText = document.createElement("p")
-    const imgOne = document.createElement("div")
-    const compCont = document.createElement("div")
-    const compText = document.createElement("p")
-    const imgTwo = document.createElement("div")
-    const userScore = document.createElement("p")
-    const compScore = document.createElement("p")
 
     userText.textContent = "Your move"
     compText.textContent = "Computer's move"
@@ -59,18 +67,13 @@ startButton.addEventListener("click", ()=> {
     compText.style.cssText = "margin: 0; color: white;"
     userText.style.fontSize = "2vh"
     compText.style.fontSize = "2vh"
-    
+
     imgOne.style.cssText = "height: 25vh; width: 25vh;"
     imgTwo.style.cssText = "height: 25vh; width: 25vh;"
-    imgOne.style.backgroundColor = "#F38C26"
-    imgTwo.style.backgroundColor = "#F38C26"
     imgOne.style.borderRadius = "1vh"
     imgTwo.style.borderRadius = "1vh"
     imgOne.style.border = "5px solid #540B05"
     imgTwo.style.border = "5px solid #540B05"
-
-    let scoreOne = 0
-    let scoreTwo = 0
 
     userScore.textContent = `Your score: ${scoreOne}`
     compScore.textContent = `Your score: ${scoreTwo}`
@@ -98,5 +101,8 @@ startButton.addEventListener("click", ()=> {
     actionDiv.style.cssText = "display:flex; gap: 50vh;"
 
     startMain.appendChild(actionDiv)
+
 })
 
+imgOne.src = "imgs/paper.png"
+imgTwo.src = "imgs/rock.png"
