6521732edbd2b1883d2205e8a3012c643f7db1a2
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-05 06:45:19 +0000

    Finished initial js

--- rps.js
+++ rps.js
@@ -9,15 +9,13 @@ startButton.addEventListener("click", ()=> {
     // changing existing ui
     startMain.removeChild(startButton)
     startMain.removeChild(startH1)
-    header.style.height = "5vh"
-    footer.style.height = "5vh"
+    header.style.height = "1vh"
+    footer.style.height = "1vh"
 
     // adding divs for new ui
     const textDiv = document.createElement("div")
     const buttonDiv = document.createElement("div")
     const actionDiv = document.createElement("div")
-    const resultDiv = document.createElement("div")
-    const scoreDiv = document.createElement("div")
 
     // adding content to new ui
     const textH1 = document.createElement("h1")
@@ -46,20 +44,58 @@ startButton.addEventListener("click", ()=> {
     buttonDiv.style.cssText = "display:flex; gap: 2vh;"
     startMain.appendChild(buttonDiv)
     
+    const userCont = document.createElement("div")
+    const userText = document.createElement("p")
     const imgOne = document.createElement("div")
+    const compCont = document.createElement("div")
+    const compText = document.createElement("p")
     const imgTwo = document.createElement("div")
+    const userScore = document.createElement("p")
+    const compScore = document.createElement("p")
+
+    userText.textContent = "Your move"
+    compText.textContent = "Computer's move"
+    userText.style.cssText = "margin: 0; color: white;"
+    compText.style.cssText = "margin: 0; color: white;"
+    userText.style.fontSize = "2vh"
+    compText.style.fontSize = "2vh"
+    
     imgOne.style.cssText = "height: 25vh; width: 25vh;"
     imgTwo.style.cssText = "height: 25vh; width: 25vh;"
     imgOne.style.backgroundColor = "#F38C26"
     imgTwo.style.backgroundColor = "#F38C26"
     imgOne.style.borderRadius = "1vh"
     imgTwo.style.borderRadius = "1vh"
-    imgOne.style.border = "5px solid black"
-    imgTwo.style.border = "5px solid black"
+    imgOne.style.border = "5px solid #540B05"
+    imgTwo.style.border = "5px solid #540B05"
 
-    actionDiv.appendChild(imgOne)
-    actionDiv.appendChild(imgTwo)
-    actionDiv.style.cssText = "display:flex; gap: 30vh;"
+    let scoreOne = 0
+    let scoreTwo = 0
+
+    userScore.textContent = `Your score: ${scoreOne}`
+    compScore.textContent = `Your score: ${scoreTwo}`
+    userScore.style.cssText = "margin: 0; color: white;"
+    compScore.style.cssText = "margin: 0; color: white;"
+    userScore.style.fontSize = "2vh"
+    compScore.style.fontSize = "2vh"
+
+    userCont.appendChild(userText)
+    userCont.appendChild(imgOne)
+    userCont.appendChild(userScore)
+    compCont.appendChild(compText)
+    compCont.appendChild(imgTwo)
+    compCont.appendChild(compScore)
+
+    userCont.style.cssText = "display: flex; gap: 1vh"
+    compCont.style.cssText = "display: flex; gap: 1vh"
+    userCont.style.flexDirection = "column"
+    compCont.style.flexDirection = "column"
+    userCont.style.alignItems = "center"
+    compCont.style.alignItems = "center"
+
+    actionDiv.appendChild(userCont)
+    actionDiv.appendChild(compCont)
+    actionDiv.style.cssText = "display:flex; gap: 50vh;"
 
     startMain.appendChild(actionDiv)
 })
